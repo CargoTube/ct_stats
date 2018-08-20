838726080059aad5f6d6752c9f673121a3b8ffb7
bwegh <baswegh@gmx.de>
2018-08-20 19:37:26 +0200

    enhance stats output

--- src/ct_stats_messages.erl
+++ src/ct_stats_messages.erl
@@ -151,7 +151,8 @@ update_with_map(#{fastest := Fastest, percentile25 := Percentile25,
                   percentile50 := Percentile50, percentile75 := Percentile75,
                   percentile99 := Percentile99, slowest := Slowest,
                   avg_msg_sec := AvgMsgSec, msg_count := MsgCount}, State) ->
-    lager:info("stats: ~p msg/sec [ ~p / ~p / ~p / ~p / * ~p * / ~p ] ms; #~p",
+    lager:info("stats: ~.2.f msg/sec [ ~.2.f / ~.2.f / ~.2.f / ~.2.f /"
+               " * ~.2.f * / ~.2.f ] ms; #~p",
                [AvgMsgSec, Fastest, Percentile25, Percentile50, Percentile75,
                 Percentile99, Slowest, MsgCount]),
     State#state{ fastest = Fastest,
